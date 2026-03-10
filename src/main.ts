5472d3a8acbb64396b30ac693bba6900c0ed0c46
quirrel-zh <f2icloevil@gmail.com>
2026-03-10 15:15:47 +0800

    refactor: remove Ant Design's `reset.css` import, with base styles now managed by `tailwind.css`

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/tailwind.css'
-import 'ant-design-vue/dist/reset.css'
 import './assets/styles/antd-overrides.less'
 
 const app = createApp(App)
